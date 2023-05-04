d9e52e9c9141c58db233a875b4a5180b7f74a8f7
vegaluh <58166179+vegaluh@users.noreply.github.com>
2023-05-04 14:04:36 +0700

    Update Logo icon web

--- head.js
+++ head.js
@@ -7,8 +7,8 @@ document.write(`<head>
 <meta content="" name="keywords">
 
 <!-- Favicons -->
-<link href="assets/img/favicon.png" rel="icon">
-<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="assets/img/Logo_HT-2-removebg-preview.png" rel="icon">
+<link href="assets/img/Logo_HT-2-removebg-preview.png" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
