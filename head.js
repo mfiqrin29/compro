d1be7f9c6181389805ca26e987b7b3380aeafc1e
vegaluh <58166179+vegaluh@users.noreply.github.com>
2023-05-05 16:41:43 +0700

    ubah title

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ document.write(`<head>
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>UpConstruction Bootstrap Template - Indextesgaluh</title>
+<title>HT International</title>
 <meta content="" name="description">
 <meta content="" name="keywords">
 
