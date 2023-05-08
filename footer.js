3edcf801e0348ff0c8f5837ecc7b49165d01f32b
Muhammad Fiqri N <50320950+mfiqrin29@users.noreply.github.com>
2023-05-08 20:51:37 +0700

    update footer

--- footer.js
+++ footer.js
@@ -1,53 +1,89 @@
-document.write(`<footer id="footer" class="footer">
+document.write(`  <footer id="footer" class="footer">
 
-    <div class="footer-content position-relative">
-      <div class="container">
-        <div class="row">
+<div class="footer-content position-relative">
+  <div class="container">
+    <div class="row">
 
-          <div class="col-lg-4 col-md-6">
-            <div class="footer-info">
-              <h3>HT International</h3>
-              <p>
-                Marquee Executive Office<br>
-                Pondok Indah Office Tower III, 17th Floor<br>
-                Jl. Sultan Iskandar Muda Kav V-TA Jakarta Selatan<br>
-                DKI Jakarta 12310 Indonesia<br><br>
-                <!-- <strong>Phone:</strong> +1 5589 55488 55<br> -->
-                <strong>Email:</strong> htinter.indo@gmail.com<br>
-              </p>
-              
-            </div>
-          </div><!-- End footer info column-->
+      <div class="col-lg-4 col-md-6">
+        <div class="footer-info">
+          <h3>HT International</h3>
+          <h6>Jakarta Office
+            Address:
+          </h6>
+          <p>
+            Marquee Executive Office<br>
+            Pondok Indah Office Tower III, 17th Floor<br>
+            Jl. Sultan Iskandar Muda Kav V-TA Jakarta Selatan<br>
+            DKI Jakarta 12310 Indonesia<br><br>
+            <!-- <strong>Phone:</strong> +1 5589 55488 55<br> -->
+            <strong>Email:</strong> htinter.indo@gmail.com<br>
+          </p>
 
-          <div class="col-lg-2 col-md-3 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-            <li><a href="blog.html">Business Line</a></li>
-              <li><a href="about.html">About us</a></li>
-              
-              <li><a href="contact.php">Contact Us</a></li>
-              
-            </ul>
-          </div><!-- End footer links column-->
-
-          
         </div>
+      </div><!-- End footer info column-->
+
+      <div class="col-lg-4 col-md-6">
+        <div class="footer-info" style="padding-top: 55px;">
+
+
+          <h6>Japan Branch:
+            Address:
+
+
+          </h6>
+          <p>
+
+            405-1 Kumiyama-cho Morimurahigashi
+            Kuze-gun Kyoto 613-0024 Japan<br>
+            <!-- <strong>Phone:</strong> +1 5589 55488 55<br> -->
+            <br>
+            <br>
+            <br>
+            <strong>Email:</strong> htinter.indo@gmail.com<br>
+          </p>
+
+        </div>
+      </div><!-- End footer info column-->
+
+
+      <div class="col-lg-2 col-md-3 footer-links" style="padding-top: 50px;">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About us</a></li>
+
+        </ul>
+      </div><!-- End footer links column-->
+      <div class="col-lg-2 col-md-3 footer-links" style="padding-top: 85px;">
+
+        <ul>
+          <li><a href="blog.html">Business Line</a></li>
+          <li><a href="contact.php">Contact</a></li>
+
+        </ul>
       </div>
-    </div>
 
-    <div class="footer-legal text-center position-relative">
-      <div class="container">
-        <div class="copyright">
-          &copy; Copyright <strong><span>HT International</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ -->
-          <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
-        </div>
-      </div>
-    </div>
 
-  </footer>`);
+
+    </div><!-- End footer links column-->
+
+  </div>
+</div>
+
+
+<div class="footer-legal text-center position-relative">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>HT International</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ -->
+      <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+    </div>
+  </div>
+</div>
+
+</footer>`);
